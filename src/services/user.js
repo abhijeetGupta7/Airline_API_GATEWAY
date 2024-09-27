ab46fd81bb4fa1f0efe1031bc344309f2d186f43
abhijeetGupta7 <127014104+abhijeetGupta7@users.noreply.github.com>
2024-09-27 19:06:50 +0530

    Added JWT expiration check

--- src/services/user.js
+++ src/services/user.js
@@ -60,6 +60,9 @@ class UserService {
             if(error.name=='JsonWebTokenError') {
                 throw new AppError("Invalid JWT",StatusCodes.BAD_REQUEST);
             }
+            if(error.name=='TokenExpiredError') {
+                throw new AppError("JWT token expired",StatusCodes.BAD_REQUEST);
+            }
             console.log(error);
             throw error;
         }
